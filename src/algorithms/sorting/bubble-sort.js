675f90ef780a9de604ccb9743186769c92aea9ad
tuhongwei <1690542111@qq.com>
2018-06-02 17:17:47 +0800

    bubble sort完成

--- src/algorithms/sorting/bubble-sort.js
+++ src/algorithms/sorting/bubble-sort.js
@@ -1,9 +1,17 @@
 /*
- *冒泡排序
+ *冒泡排序-从小到大
  */
 function bubbleSort(arr){
 	var l = arr.length;
-	fot(var i=l; i>0; i--){
-		for(var j=)
+	for(var i=l-1; i>1; i--){
+		for(var j=0; j<i; j++){
+			if(arr[j] > arr[j+1]){
+				var temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
 	}
 }
+/*
+ *时间复杂度T(n) = O((n-2)*(n-1)) = O(n^2)
