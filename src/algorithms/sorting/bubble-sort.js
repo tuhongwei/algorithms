2d8c0f254dbaa734b8f1e2477273c903e40877d2
李晓云 <lixiaoyun@lixiaoyundeMacBook-Pro.local>
2018-12-15 06:44:46 +0800

    up bubble-sort.js

--- src/algorithms/sorting/bubble-sort.js
+++ src/algorithms/sorting/bubble-sort.js
@@ -3,7 +3,7 @@
  */
 function bubbleSort(arr){
 	var l = arr.length;
-	for(var i=l-1; i>1; i--){
+	for(var i=l-1; i>=1; i--){
 		for(var j=0; j<i; j++){
 			if(arr[j] > arr[j+1]){
 				var temp = arr[j];
@@ -15,5 +15,5 @@ function bubbleSort(arr){
 	return arr;
 }
 /*
- *时间复杂度T(n) = O((n-2)*(n-1)) = O(n^2)
+ *时间复杂度T(n) = O((n-1)*(n-2)) = O(n^2)
  */
